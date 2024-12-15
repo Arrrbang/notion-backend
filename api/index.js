0110a5f8028029cfd937dc1cc026c50ff4d22f9b
Arrrbang <arem0602@gmail.com>
2024-12-15 14:45:08 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,22 +3,18 @@ const cors = require('cors');
 
 const app = express();
 
-// 기존 CORS 설정을 동적 설정으로 변경
 app.use(cors((req, callback) => {
-  const whitelist = ['https://arrrbang.github.io']; // 허용할 출처 리스트
+  const whitelist = ['https://arrrbang.github.io'];
   const origin = req.header('Origin');
   if (whitelist.includes(origin)) {
-    callback(null, { origin: true }); // 요청 출처 허용
+    callback(null, { origin: true });
   } else {
-    callback(null, { origin: false }); // 허용되지 않은 출처
+    callback(null, { origin: false });
   }
 }));
 
-
-
 app.use(express.json());
 
-// 테스트용 라우트
 app.get('/notion', (req, res) => {
   res.json({
     success: true,
