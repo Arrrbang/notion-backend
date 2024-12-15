817017bb649a329ab17d07150e9220e6a09e39ee
Arrrbang <arem0602@gmail.com>
2024-12-15 14:54:15 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,19 +3,21 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors((req, callback) => {
-  const whitelist = ['https://arrrbang.github.io'];
-  const origin = req.header('Origin');
-  if (whitelist.includes(origin)) {
-    callback(null, { origin: true });
-  } else {
-    callback(null, { origin: false });
-  }
+// CORS 설정: 모든 출처 허용 (개발용)
+app.use(cors({
+  origin: 'https://arrrbang.github.io',
+  methods: ['GET', 'POST', 'OPTIONS'], // OPTIONS 메서드 허용
+  allowedHeaders: ['Content-Type', 'Authorization'] // 헤더 허용
 }));
+
+// Preflight 요청에 대한 응답 설정
+app.options('/notion', cors()); // OPTIONS 요청에 CORS 응답
 
 app.use(express.json());
 
+// 테스트 라우트
 app.get('/notion', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://arrrbang.github.io');
   res.json({
     success: true,
     data: [
